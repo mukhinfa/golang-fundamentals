6d5e6383fb1bdc40631186052d636d8cdb16df29
Fedor Mukhin <muhinfa@gmail.com>
2024-10-12 01:58:36 +0300

    Задание - HTTP запросы

--- 3-bin/api/api.go
+++ 3-bin/api/api.go
@@ -1,7 +1,83 @@
 package api
 
-import "golang_fundamentals/3-bin/config"
+import (
+	"golang_fundamentals/3-bin/config"
+
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const genUrl = "https://api.jsonbin.io/v3/b"
 
 type Api struct {
 	config config.Config
+}
+
+func NewRequest() *Api {
+	return &Api{
+		config: *config.NewConfig(),
+	}
+
+}
+
+func (c Api) Post(bin []byte) ([]byte, error) {
+	baseUrl := genUrl
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, baseUrl, bytes.NewBuffer(bin))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:")
+	}
+	return request(req, client, c)
+}
+
+func (c Api) Get(id string) ([]byte, error) {
+	baseUrl := genUrl + "/" + id
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, baseUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating HTTP request:")
+	}
+	return request(req, client, c)
+}
+
+func (c Api) Put(bin []byte, id string) ([]byte, error) {
+	baseUrl := genUrl + "/" + id
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, baseUrl, bytes.NewBuffer(bin))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:")
+	}
+	return request(req, client, c)
+}
+
+func (c Api) Delete(id string) ([]byte, error) {
+	baseUrl := genUrl + "/" + id
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodDelete, baseUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating HTTP request:")
+	}
+	return request(req, client, c)
+}
+
+func request(req *http.Request, client *http.Client, c Api) ([]byte, error) {
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Master-Key", c.config.Key)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error creating HTTP request:")
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Ошибка чтения ответа")
+		return nil, err
+	}
+	return body, nil
 }
--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,12 +1,235 @@
 package main
 
-import "golang_fundamentals/3-bin/bins"
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"golang_fundamentals/3-bin/api"
+	"golang_fundamentals/3-bin/bins"
+	"golang_fundamentals/3-bin/file"
+	"golang_fundamentals/3-bin/storage"
+)
 
-type Db interface {
-	Read() (*bins.BinList, error)
-	Save(binList bins.BinList)
+type responsePost struct {
+	Metadata bins.Bin `json:"metadata"`
+}
+
+type responsePut struct {
+	Metadata struct {
+		ParentId string `json:"parentId"`
+		Private  bool   `json:"private"`
+	} `json:"metadata"`
+}
+
+type operationData struct {
+	name        string
+	id          string
+	binFileName string
+	FileDb
+	HttpRequest
+}
+
+func newOperationData(name, id, binFileName string, fileDb FileDb, httpRequest HttpRequest) *operationData {
+	return &operationData{
+		name:        name,
+		id:          id,
+		binFileName: binFileName,
+		FileDb:      fileDb,
+		HttpRequest: httpRequest,
+	}
+}
+
+type FileDb interface {
+	Read() ([]byte, error)
+	Write(content []byte)
+}
+
+type HttpRequest interface {
+	Post([]byte) ([]byte, error)
+	Get(string) ([]byte, error)
+	Put([]byte, string) ([]byte, error)
+	Delete(string) ([]byte, error)
 }
 
 func main() {
+	var name, id, fileName string
 
+	flag.StringVar(&name, "name", "", "Имя бина")
+	flag.StringVar(&id, "id", "", "bin id")
+	flag.StringVar(&fileName, "file", "", "Имя файла")
+
+	create := flag.Bool("create", false, "create bin")
+	update := flag.Bool("update", false, "update bin")
+	delete := flag.Bool("delete", false, "delete bin")
+	get := flag.Bool("get", false, "delete bin")
+	list := flag.Bool("list", false, "bin list bin")
+
+	flag.Parse()
+
+	od := newOperationData(
+		name,
+		id,
+		"binStorage/"+fileName,
+		file.NewJsonDB("binList.json"),
+		api.NewRequest(),
+	)
+
+	switch {
+	case *create:
+		od.createBin()
+	case *update:
+		od.updateBin()
+	case *delete:
+		od.deleteBin()
+	case *get:
+		od.getBin()
+	case *list:
+		od.listBin()
+	}
+
+}
+
+func (od *operationData) createBin() {
+	fmt.Println("Создание бина")
+
+	data, err := od.Read()
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:")
+	}
+
+	resp, err := od.Post(data)
+	if err != nil {
+		fmt.Println("Ошибка запроса")
+	}
+
+	var respData responsePost
+	err = json.Unmarshal(resp, &respData)
+	if err != nil {
+		fmt.Println("Ошибка анмаршаллинга", err.Error())
+	}
+
+	bin := bins.CreateBin(respData.Metadata.Id, od.name, respData.Metadata.Private)
+	storageDb := storage.NewStorageDb("binList.json", file.NewJsonDB("binList.json"))
+	binList, err := storageDb.Read()
+	if err != nil {
+		fmt.Println("Ошибка чтения Бинлиста")
+	}
+
+	binList.Bins = append(binList.Bins, *bin)
+	err = storageDb.Save(*binList)
+	if err != nil {
+		fmt.Println("Ошибка сохранения Бинлиста")
+	}
+
+	fmt.Println("Bin создан")
+}
+
+func (od *operationData) updateBin() {
+	fmt.Println("Обновление бина")
+
+	data, err := od.Read()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	resp, err := od.Put(data, od.id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var respData responsePut
+	err = json.Unmarshal(resp, &respData)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	storageDb := storage.NewStorageDb("binList.json", file.NewJsonDB("binList.json"))
+	binList, err := storageDb.Read()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for i, v := range binList.Bins {
+		if v.Id == od.id {
+			updatedBin := bins.CreateBin(respData.Metadata.ParentId, v.Name, respData.Metadata.Private)
+			arr := append(binList.Bins[:i], *updatedBin)
+			arr = append(arr, binList.Bins[i+1:]...)
+			binList.Bins = arr
+			break
+		}
+	}
+	err = storageDb.Save(*binList)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Bin обновлен")
+}
+
+func (od *operationData) deleteBin() {
+	fmt.Println("Удаление бина")
+
+	_, err := od.Delete(od.id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	storageDb := storage.NewStorageDb("binList.json", file.NewJsonDB("binList.json"))
+	binList, err := storageDb.Read()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for i, v := range binList.Bins {
+		if v.Id == od.id {
+			binList.Bins = append(binList.Bins[:i], binList.Bins[i+1:]...)
+			break
+		}
+	}
+
+	err = storageDb.Save(*binList)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Bin удален")
+}
+
+func (od *operationData) getBin() {
+	fmt.Println("Получение бина")
+
+	resp, err := od.Get(od.id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	result, err := prettyString(resp)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Bin %s\n%s", od.id, result)
+}
+
+func (od *operationData) listBin() {
+	fmt.Println("Список бинов")
+
+	storageDb := storage.NewStorageDb("binList.json", file.NewJsonDB("binList.json"))
+	binList, err := storageDb.Read()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, v := range binList.Bins {
+		fmt.Printf("id %s name %s\n", v.Id, v.Name)
+	}
+}
+
+func prettyString(str []byte) (string, error) {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, str, "", "  "); err != nil {
+		return "", err
+	}
+	return prettyJSON.String(), nil
 }
